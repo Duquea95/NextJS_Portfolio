d40810545108150e79e14817118b06510005917f
Anthony <duquea95@gmail.com>
2022-10-27 05:41:40 -0400

    styling udpates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a5ff043f8785f8cb"; }
+/******/ 		__webpack_require__.h = function() { return "8f997291263b8483"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
