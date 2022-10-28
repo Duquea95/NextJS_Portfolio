9d2502fb25db8f822c54e321493ea073d6f4440b
Anthony <duquea95@gmail.com>
2022-10-28 13:33:22 -0400

    fix icon border

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import anthony from'../../public/images/anthony.jpg' 
 import Header from "./Header"
 import Footer from "./Footer"
 
@@ -7,6 +8,7 @@ export default function Layout({ children }) {
     <>
       {/* <Header> */}
       <Head>
+        <meta property="og:image" content={anthony} />
         <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet"></link>
       </Head>
       <main>{children}</main>
