353e2df8c39c2ef4aea8047ae090ca13e9305525
Anthony <duquea95@gmail.com>
2022-11-02 07:18:04 -0400

    updates

--- pages/PdfReader/index.js
+++ pages/PdfReader/index.js
@@ -1,35 +1,10 @@
-import React, { useState } from "react";
-import { Document, Page } from "react-pdf";
 import resumePDF from '../../public/myResume.pdf';
 
 const PdfReader = () => {
 
-  const PdfPage = (props) => {
-      const [numPages, setNumPages] = useState(null);
-      const [pageNumber, setPageNumber] = useState(1); //setting 1 to show fisrt page
-
-      function onDocumentLoadSuccess({ numPages }) {
-        setNumPages(1);
-        setPageNumber(1);
-      }
-
-      const { pdf } = props;
-      
-    return(
-      <>
-        <Document
-          file={pdf}
-          options={{ workerSrc: "/pdf.worker.js" }}
-          onLoadSuccess={onDocumentLoadSuccess}
-        >
-        </Document>
-      </>
-    )
-  }
-
   return (
     <>
-      <PdfPage pdf={resumePDF} />
+      <iframe src={resumePDF} />
     </>
   );
 }
