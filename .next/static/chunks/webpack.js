b97ae269357715d119c9ce126d21d50858c1e9ee
Anthony <duquea95@gmail.com>
2023-01-14 14:06:50 -0500

    css updates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e7b6ccff06be77d6"; }
+/******/ 		__webpack_require__.h = function() { return "54405fd1a9c243a8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
